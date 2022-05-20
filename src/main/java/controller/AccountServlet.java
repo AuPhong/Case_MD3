1d2b39a7bd153c0b97fa37759ec71a463fa1b1fb
AuPhong <stthu98@gmail.com>
2022-05-20 14:48:02 +0700

    phong

---
+++ src/main/java/controller/AccountServlet.java
@@ -0,0 +1,26 @@
+package controller;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+import java.io.IOException;
+
+@WebServlet(name = "AccountServlet", value = "/accounts")
+public class AccountServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String action = request.getParameter("action");
+        if (action==null){
+            action = "";
+        }
+        switch (action){
+
+        }
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+}
