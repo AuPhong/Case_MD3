c9233b28fd8e5840bdfbaea6ec5a9e4b122b3223
AuPhong <stthu98@gmail.com>
2022-05-20 15:46:50 +0700

    commit

--- src/main/java/controller/AccountServlet.java
+++ src/main/java/controller/AccountServlet.java
@@ -10,20 +10,24 @@ public class AccountServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         String action = request.getParameter("action");
-        if (action==null){
+        if (action == null) {
             action = "";
         }
-        switch (action){
+        switch (action) {
+            case "login":
+
             default:
-                showHomePage(request,response);
+                showLoginPage(request, response);
                 break;
         }
 
+
     }
 
-    private void showHomePage(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        request.getRequestDispatcher("home/productList.jsp").forward(request,response);
+    private void showLoginPage(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        request.getRequestDispatcher("account/login.jsp").forward(request, response);
     }
+
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
